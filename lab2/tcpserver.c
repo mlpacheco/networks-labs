5e3b352b06979f756282aff6233366b7d00e0da6
eme-ele <emele18@gmail.com>
2016-09-24 17:40:49 -0400

    starting lab2

---
+++ lab2/tcpserver.c
@@ -0,0 +1,25 @@
+// Generic socket
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// Generic system
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/signal.h>
+
+int main(int argc, char *argv[]) {
+
+    pid_t k;
+
+    while(1) {
+
+        
+
+    }
+
+}
