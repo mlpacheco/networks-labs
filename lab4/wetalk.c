fb1702226eed173d4871501e7fcff68cb2b09f25
ML <emele18@gmail.com>
2016-10-23 22:42:49 -0400

    chat can be established between 2 peers

--- lab4/wetalk.c
+++ lab4/wetalk.c
@@ -6,15 +6,45 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <signal.h>
+#include <pthread.h>
 
 #define MAX_BUFF 1000
 
 volatile sig_atomic_t keep_prompting = 0;
+struct sockaddr_in addr_rcv;
+struct sockaddr_in addr_snd;
+int sd_snd, sd_rcv;
+int chatting = 0;
+int quit = 0;
+int was_asked = 0;
+
 
 // signal handler to terminate client if no response
 void catch_alarm (int sig) {
     printf("No response\n");
     keep_prompting = 1;
+}
+
+void *listen_connections(void *threadid) {
+    char buffer[MAX_BUFF + 1];
+    socklen_t addrsize = sizeof(addr_rcv);
+
+    while(1) {
+        if (chatting)
+            pthread_exit(NULL);
+        long tid;
+        tid = (long) threadid;
+        int len_rcv = recvfrom(sd_rcv, buffer, sizeof(buffer), 0, (struct sockaddr *) &addr_rcv, &addrsize);
+        if (len_rcv > 0) {
+            buffer[len_rcv] = '\0';
+            if (strcmp(buffer, "wannatalk") == 0) {
+                printf("\n| chat request from %s %d\n", inet_ntoa(addr_rcv.sin_addr), ntohs(addr_rcv.sin_port));
+                was_asked = 1;
+            }
+        }
+    }
+
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,9 +56,7 @@ int main(int argc, char *argv[]) {
     }
 
     // variables needed
-    int port_rcv, port_snd, sd_rcv, sd_snd, len_rcv, chat;
-    struct sockaddr_in addr_rcv;
-    struct sockaddr_in addr_snd;
+    int port_rcv, port_snd, len_rcv;
     char buffer[MAX_BUFF + 1];
     char * inp_hostname;
     char * inp_port;
@@ -68,46 +96,77 @@ int main(int argc, char *argv[]) {
     sa.sa_handler = catch_alarm;
     sigaction (SIGALRM, &sa, 0);
 
-    chat = 0;
-    while (!chat) {
+    pthread_t thread;
+    pthread_create(&thread, NULL, listen_connections, 0);
+
+    while (!chatting) {
         // print prompt
         printf("? ");
         fgets(buffer, sizeof buffer, stdin);
-        //printf("%s", buffer);
+        //printf("buffer %s buffer_size %lu\n", buffer, strlen(buffer));
+        if (strlen(buffer) == 2) {
+            if (strcmp(buffer, "q\n") == 0) {
+                quit = 1;
+                break;
+            } else if (strcmp(buffer, "c\n") == 0 && was_asked) {
+                sendto(sd_rcv, "OK", 2, 0, (struct sockaddr *) &addr_rcv, sizeof(addr_rcv));
+                chatting = 1;
+                continue;
+            } else if (strcmp(buffer, "n\n") == 0 && was_asked) {
+                sendto(sd_rcv, "KO", 2, 0, (struct sockaddr *) &addr_rcv, sizeof(addr_rcv));
+                continue;
+            } else {
+                printf("Unknown command\n");
+                continue;
+            }
 
-        // parse input hostname (or q)
-        inp_hostname = strtok(buffer, " ");
-        if (strcmp(inp_hostname, "q\n") == 0)
-            break;
-        else
+        } else {
+            // parse hostname and port
+            inp_hostname = strtok(buffer, " ");
             ipv4address = gethostbyname(inp_hostname);
+            if (!ipv4address) {
+                printf("Unknown host\n");
+                continue;
+            }
 
-        if (!ipv4address) {
-            printf("Unknown host\n");
-            continue;
+            inp_port = strtok(NULL, "\n");
+            port_snd = atoi(inp_port);
+
+            // set info to communicate with specified host and port
+            memset(&addr_snd, 0, sizeof(addr_snd));
+            addr_snd.sin_family = AF_INET;
+            bcopy ( ipv4address->h_addr, &(addr_snd.sin_addr.s_addr), ipv4address->h_length);
+            addr_snd.sin_port = htons(port_snd);
+
+            // send message
+            ualarm(7000000, 0);
+            sendto(sd_snd, "wannatalk", 9, 0, (struct sockaddr *) &addr_snd, sizeof(addr_snd));
+            len_rcv = recvfrom(sd_snd, buffer, sizeof(buffer), 0, (struct sockaddr *) &addr_snd, &addrsize);
+            // cancel timeout
+            ualarm(0, 0);
+
+            // break out of the loop if user wants to chat
+            if (len_rcv > 0) {
+                buffer[len_rcv] = '\0';
+                if (strcmp(buffer, "OK") == 0) {
+                    chatting = 1;
+                    continue;
+                } else if (strcmp(buffer, "KO") == 0) {
+                    printf("| doesn't want to chat\n");
+                    continue;
+                }
+            }
+
         }
 
-        // parse port
-        inp_port = strtok(NULL, "\n");
-        port_snd = atoi(inp_port);
+    }
 
-        // set info to communicate with specified host and port
-        memset(&addr_snd, 0, sizeof(addr_snd));
-        addr_snd.sin_family = AF_INET;
-        bcopy ( ipv4address->h_addr, &(addr_snd.sin_addr.s_addr), ipv4address->h_length);
-        addr_snd.sin_port = htons(port_snd);
+    if (quit) {
+        printf("Goodbye!\n");
+    }
 
-        // send message
-        ualarm(7000000, 0);
-        sendto(sd_snd, "wannatalk", 9, 0, (struct sockaddr *) &addr_snd, sizeof(addr_snd));
-        len_rcv = recvfrom(sd_rcv, buffer, sizeof(buffer), 0, (struct sockaddr *) &addr_rcv, &addrsize);
-
-        // break out of the loop if user wants to chat
-        if (len_rcv > 0) {
-            buffer[len_rcv] = '\0';
-            if (strcmp(buffer, "OK") == 0)
-                chat = 1;
-        }
+    if (chatting) {
+        printf("we are chatting!\n");
     }
 
 }
