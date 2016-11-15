bad1e607efe05309e5b69b26fb12e1697aafe01e
eme-ele <emele18@gmail.com>
2016-11-14 20:13:46 -0500

    stable form

--- lab5/audiolisten.c
+++ lab5/audiolisten.c
@@ -5,20 +5,263 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <pthread.h>
 
 #define MAX_BUFF 1000
+
+// socket we need to share we signals
+int udp_sd, buf_sz, fd, payload_size, target_buf;
+double gamma_;
+int last_read = 0;
+int last_written = 0;
+int occupied = 0;
+
+// flags to control program in and out of signals
+volatile sig_atomic_t sigpoll_signal = 0;
+volatile sig_atomic_t alarm_signal = 0;
+
+// shared buffer and semaphore to control shared access
+char * shared_buff;
+sem_t mutex;
+
+void alarm_handl(int sig) {
+    //printf("Got alarm signal\n");
+    alarm_signal = 1;
+}
+
+void sigpoll_handl(int sig) {
+    //printf("Got sigpoll signal\n");
+    sigpoll_signal = 1;
+}
+
+void *playback(void *threadid) {
+    // read from buffer, write to dev/audio
+    // shared buff is a circular buffer
+    char * out;
+    int num_bytes;
+
+    sem_wait(&mutex);
+    if (last_written > last_read) {
+        num_bytes = last_written - last_read;
+        out = malloc((num_bytes + 1) * sizeof(char));
+        int i;
+        int j = 0;
+        for(i = last_read + 1; i <= last_written; i++) {
+            out[j] = shared_buff[i];
+            j++;
+            last_read = i;
+        }
+    } else if (last_written < last_read) {
+        num_bytes = buf_sz - last_read + last_written;
+        out = malloc((num_bytes + 1)* sizeof(char));
+        int i;
+        int j = 0;
+        for (i = last_read + 1; i < buf_sz; i++) {
+            out[j] = shared_buff[i];
+            j++;
+            last_read = i;
+        }
+
+        for (i = 0; i <= last_written; i++) {
+            out[j] = shared_buff[i];
+            j++;
+            last_read = i;
+        }
+    } else {
+        num_bytes = 0;
+        out = malloc((num_bytes + 1) * sizeof(char));
+    }
+    // if last written is last read there is nothing to read
+    sem_post(&mutex);
+
+    out[num_bytes] = '\0';
+    if (num_bytes > 0) {
+        printf("last read: %d, num bytes: %d out len: %lu\n",
+                last_read, num_bytes, strlen(out));
+        write(fd, out, strlen(out));
+    }
+    pthread_exit(NULL);
+}
+
+void *read_stream(void *threadid) {
+    struct sockaddr_in server_addr;
+    int addr_len = sizeof(server_addr);
+    char buffer[MAX_BUFF + 1];
+    char sequence[4 + 1];
+    char payload[payload_size + 1];
+    int num_bytes;
+
+    num_bytes = recvfrom(udp_sd, buffer, sizeof buffer, 0,
+                         (struct sockaddr *)&server_addr, &addr_len);
+    if (num_bytes > 0) {
+        buffer[num_bytes] = '\0';
+        printf("received %d bytes\n", num_bytes);
+        // strip sequence number
+        // what to do with the sequence
+        // how to do resequencing?
+        memcpy(sequence, &buffer[0], 4);
+        sequence[4] = '\0';
+        // get payload
+        memcpy(payload, &buffer[4], payload_size);
+        payload[payload_size] = '\0';
+
+        int free_space;
+        printf("payload %lu bytes\n", strlen(payload));
+
+        sem_wait(&mutex);
+        printf("last_read: %d, last_written: %d\n", last_read, last_written);
+        if (last_written > last_read) {
+            free_space = buf_sz - last_written + last_read;
+            occupied = buf_sz - free_space;
+            int i;
+            int j = 0;
+            for (i = last_written + 1; i < buf_sz; i++) {
+                if (j >= strlen(payload))
+                  break;
+                shared_buff[i] = payload[j];
+                j++;
+                occupied++;
+                last_written = i;
+            }
+            for (i = 0; i <= last_read; i++) {
+                if (j >= strlen(payload))
+                  break;
+                shared_buff[i] = payload[j];
+                j++;
+                occupied++;
+                last_written = i;
+            }
+        } else if (last_written < last_read) {
+            free_space = last_read - last_written;
+            occupied = buf_sz - free_space;
+            int i;
+            int j = 0;
+            for (i = last_written + 1; i <= last_read; i++) {
+                if (j >= strlen(payload))
+                  break;
+                shared_buff[i] = payload[j];
+                j++;
+                occupied++;
+                last_written = i;
+            }
+        } else if (last_written == last_read) {
+            free_space = buf_sz;
+            occupied = buf_sz - free_space;
+            int i;
+            int j = 0;
+            for (i = last_written + 1; i < buf_sz; i++) {
+                if (j >= strlen(payload))
+                    break;
+                shared_buff[i] = payload[j];
+                j++;
+                occupied++;
+                last_written = i;
+            }
+            for (i = 0; i <= last_read; i++) {
+                if (j >= strlen(payload))
+                    break;
+                shared_buff[i] = payload[j];
+                j++;
+                occupied++;
+                last_written = i;
+            }
+        }
+        sem_post(&mutex);
+
+        // send feedback
+        char feedback[MAX_BUFF + 1];
+
+        snprintf(feedback, sizeof feedback, "Q %d %d %f",
+                 occupied, target_buf, gamma_);
+        printf("%s\n", feedback);
+        printf("last written: %d\n", last_written);
+        sendto(udp_sd, feedback, strlen(feedback), 0,
+               (struct sockaddr *)&server_addr, sizeof server_addr);
+    }
+
+    pthread_exit(NULL);
+
+}
+
+int listen_stream(int udp_port, double gamma_) {
+    printf("listen_stream\n");
+    struct sockaddr_in udp_addr;
+    memset(&udp_addr, 0, sizeof udp_addr);
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    udp_addr.sin_port = htons(udp_port);
+    printf("udp port %d\n", udp_port);
+
+    // create socket for listening to stream
+    if ((udp_sd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket error");
+        return -1;
+    }
+
+    // bind listening socket
+    printf("bind udp socket %d\n", udp_sd);
+    if (bind(udp_sd, (struct sockaddr *)&udp_addr, sizeof udp_addr) < 0) {
+        perror("bind error");
+        return -1;
+    }
+
+    //stuff to handle sigpoll signals
+    struct sigaction sa_sigpoll;
+    memset(&sa_sigpoll, 0, sizeof sa_sigpoll);
+    sa_sigpoll.sa_handler = sigpoll_handl;
+    //sa_sigpoll.sa_flags = SA_NODEFER;
+    sigaction(SIGPOLL, &sa_sigpoll, 0);
+
+    fcntl(udp_sd, F_SETOWN, getpid());
+    fcntl(udp_sd, F_SETFL, FASYNC);
+
+    //stuff to handle alarm signals
+    struct sigaction sa_alarm;
+    memset(&sa_alarm, 0, sizeof sa_alarm);
+    //sa_alarm.sa_flags = SA_NODEFER;
+    sa_alarm.sa_handler = alarm_handl;
+    sigaction(SIGALRM, &sa_alarm, 0);
+
+
+
+    // open audio dev
+    fd = open("/dev/audio", O_RDWR);
+
+    // create semaphore
+    sem_init(&mutex, 0, 1);
+
+    // msec to microsec and set alarm
+    ualarm(1.0/gamma_ * 1000, 1.0/gamma_ * 1000);
+    while (1) {
+        pthread_t t1;
+        pthread_t t2;
+        if (sigpoll_signal == 1) {
+            sigpoll_signal = 0;
+            pthread_create(&t1, NULL, read_stream, 0);
+        } else if (alarm_signal == 1) {
+            alarm_signal = 0;
+            pthread_create(&t2, NULL, playback, 0);
+        }
+    }
+
+    sem_destroy(&mutex); // destroy semaphore
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
 
      if (argc != 11) {
         printf("Run: %s %s %s %s %s %s %s %s %s %s %s\n", argv[0],
                "[server-ip]", "[server-tcp-port]", "[client-udp-port]",
-               "[payload-size]", "[playback-del]", "[gamma]", "[buf-sz]",
+               "[payload-size]", "[playback-del]", "[gamma_]", "[buf-sz]",
                "[target-buf]", "[logfile-c]", "[filename]");
         return -1;
-    }
 
-    int tcp_server_sd, tcp_server_port;
+    }
+    int tcp_server_sd, tcp_server_port, udp_port;
     struct sockaddr_in tcp_server_addr;
     struct in_addr host_addr;
     struct hostent *server_ipv4_addr;
@@ -26,8 +269,25 @@ int main(int argc, char *argv[]) {
     char buffer[MAX_BUFF + 1];
 
     int num_bytes;
+    char * response;
 
     tcp_server_port = atoi(argv[2]);
+    udp_port = atoi(argv[3]);
+
+    // other input params
+    double playback_del;
+    char * logfile;
+
+    // set size of shared buffer
+    shared_buff = malloc(buf_sz * sizeof(char));
+
+    payload_size = atoi(argv[4]);
+    playback_del = atof(argv[5]);
+    gamma_ = atof(argv[6]);
+    buf_sz = atoi(argv[7]);
+    target_buf = atoi(argv[8]);
+    logfile = argv[9];
+
     printf("IP address %s\n", argv[1]);
     inet_pton(AF_INET, argv[1], &host_addr);
     server_ipv4_addr =
@@ -63,11 +323,19 @@ int main(int argc, char *argv[]) {
     sprintf(message, "%s %s", argv[3], argv[10]);
     printf("Message: %s\n", message);
 
+
     write(tcp_server_sd, message, strlen(message));
     num_bytes = read(tcp_server_sd, buffer, MAX_BUFF);
     if (num_bytes > 0) {
         buffer[num_bytes] = '\0';
-        printf("Received: %s\n", buffer);
+        response = strtok(buffer, " ");
+        if (strcmp(response, "OK") == 0) {
+            listen_stream(udp_port, gamma_);
+        } else if (strcmp(response, "KO") == 0) {
+            printf("Request denied\n");
+        } else {
+            printf("Unexpected response\n");
+        }
     }
 
     close(tcp_server_sd);
--- lab5/audiostreamd.c
+++ lab5/audiostreamd.c
@@ -5,8 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 
 #define MAX_BUFF 10000
+
+int udp_sd, mode;
+double tau;
+
 
 // signal handler to not block the server waiting
 // for child processes
@@ -15,9 +21,62 @@ void childsig_handler(int signum) {
     wait(&status);
 }
 
-int stream(char * filename, int udp_port, int payload_size,
-           double packet_spacing, int mode, char * logfile) {
+void sigpoll_handler(int signum) {
+    struct sockaddr_in client_addr;
+    int addr_len = sizeof(client_addr);
+    char buffer[MAX_BUFF + 1];
+    int num_bytes;
 
+    char * response;
+    char * Qt_str;
+    char * Qopt_str;
+    char * gamma_str;
+
+    int Qt, Qopt;
+    double gamma;
+
+    num_bytes = recvfrom(udp_sd, buffer, sizeof buffer, 0,
+                         (struct sockaddr *)&client_addr, &addr_len);
+
+    if (num_bytes > 0) {
+        buffer[num_bytes] = '\0';
+        printf("%s\n", buffer);
+        if (buffer[0] == 'Q') {
+            response = strtok(buffer, " ");
+            Qt_str = strtok(NULL, " ");
+            Qopt_str = strtok(NULL, " ");
+            gamma_str = strtok(NULL, " ");
+            Qt = atoi(Qt_str);
+            Qopt = atoi(Qopt_str);
+            gamma = atof(gamma_str);
+
+
+            // Method A
+            if (mode == 0) {
+                double a = 10;
+                if (Qt < Qopt) {
+                    double new_tau = tau - a;
+                    if (new_tau < 0)
+                        tau = 0.0;
+                    else
+                        tau = new_tau;
+                } else if (Qt > Qopt) {
+                    double new_tau = tau + a;
+                    if (new_tau < 0)
+                        tau = 0.0;
+                    else
+                        tau = new_tau;
+                }
+                // if they are equal leave current tau
+            }
+
+        }
+    }
+}
+
+int stream(char * filename, int udp_port, char * client_ip, int payload_size,
+           char * logfile) {
+    printf("Starting to stream...\n");
     int num_bytes;
     char buffer[MAX_BUFF + 1];
     char message[MAX_BUFF + 1];
@@ -26,13 +85,62 @@ int stream(char * filename, int udp_port, int payload_size,
     // four byte sequence number
     uint32_t seq;
 
+    // vars for communication
+    struct sockaddr_in client_addr;
+    struct in_addr host_addr;
+    struct hostent *ipv4address;
+    socklen_t addrsize = sizeof(client_addr);
+
+    // resolve address
+    inet_pton(AF_INET, client_ip, &host_addr);
+    ipv4address = gethostbyaddr(&host_addr, sizeof host_addr, AF_INET);
+
+    // create udp socket for streaming
+    if ((udp_sd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket error");
+        return -1;
+    }
+
+    // set info for streaming
+    memset(&client_addr, 0, sizeof client_addr);
+    client_addr.sin_family = AF_INET;
+    bcopy(ipv4address->h_addr, &(client_addr.sin_addr.s_addr),
+          ipv4address->h_length);
+    client_addr.sin_port = htons(udp_port);
+
+    // set signal to receive feedback
+    struct sigaction sa_sigpoll;
+    memset(&sa_sigpoll, 0, sizeof sa_sigpoll);
+    sa_sigpoll.sa_handler = sigpoll_handler;
+    sa_sigpoll.sa_flags = SA_NODEFER;
+    sigaction(SIGPOLL, &sa_sigpoll, 0);
+
+    fcntl(udp_sd, F_SETOWN, getpid());
+    fcntl(udp_sd, F_SETFL, FASYNC);
+
+    // open audiofile and read bytes payload_size by payload_size
     f_stream = fopen(filename, "rb");
     while ((num_bytes = fread(buffer, 1, payload_size, f_stream)) > 0) {
         buffer[num_bytes] = '\0';
         sprintf(message, "%d%s", seq, buffer);
+        sendto(udp_sd, message, strlen(message), 0,
+               (struct sockaddr *)&client_addr, sizeof client_addr);
+        //printf("Sent packet %d\n", seq);
+        // space packets, convert msec to microsec
+        // for now its fixed, this must change
+        // we might need a global variable
+        double excess = tau;
+        printf("Waiting: %f\n", tau);
+        while (excess > 1000) {
+            usleep(1000 * 1000);
+            excess -= 1000;
+        }
+        usleep(excess * 1000);
+        seq += 1;
 
     }
-    // stream
+    printf("Finished streaming\n");
+
     return 1;
 }
 
@@ -54,15 +162,17 @@ int main(int argc, char *argv[]) {
 
     // incomming client info
     struct sockaddr_in client_addr;
+    char * client_ip;
+    socklen_t addrsize;
 
     // other input params
-    int udp_port, payload_size, mode;
-    double packet_spacing;
+    int udp_port, payload_size;
     char * logfile;
 
     udp_port = atoi(argv[2]);
     payload_size = atoi(argv[3]);
-    packet_spacing = atof(argv[4]);
+    // initial packet spacing
+    tau = atof(argv[4]);
     mode = atoi(argv[5]);
     logfile = argv[6];
 
@@ -76,6 +186,7 @@ int main(int argc, char *argv[]) {
     tcp_addr.sin_family = AF_INET;
     tcp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     tcp_addr.sin_port = htons(tcp_port);
+    printf("tcp port %d\n", tcp_port);
 
     // create socket to listen to requests
     if ((tcp_sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -84,6 +195,7 @@ int main(int argc, char *argv[]) {
     }
 
     // bind listening socket
+    printf("bind tcp socket %d\n", tcp_sd);
     if ((bind(tcp_sd, (struct sockaddr *)&tcp_addr, sizeof(tcp_addr))) < 0) {
         perror("bind error");
         return -1;
@@ -100,8 +212,10 @@ int main(int argc, char *argv[]) {
 
 
     while (1) {
-        tcp_conn = accept(tcp_sd, (struct sockaddr *)&client_addr, 0);
-        char * client_ip = inet_ntoa(client_addr.sin_addr);
+        addrsize = sizeof(client_addr);
+        tcp_conn = accept(tcp_sd, (struct sockaddr *)&client_addr, &addrsize);
+        client_ip = inet_ntoa(client_addr.sin_addr);
+        printf("Received request from: %s\n", client_ip);
 
         num_bytes = read(tcp_conn, buffer, MAX_BUFF);
         if (num_bytes > 0) {
@@ -121,8 +235,9 @@ int main(int argc, char *argv[]) {
                 if (k == 0) {
                     // what is the different between the two ports ?
                     udp_port = atoi(portnumber);
-                    stream(filename, udp_port, payload_size, packet_spacing,
-                           mode, logfile);
+                    stream(filename, udp_port, client_ip, payload_size,
+                           logfile);
+                    exit(0);
 
                 } else {
                     close(tcp_conn);
