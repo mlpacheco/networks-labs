61bc547f77a1d279b435707b446a88c00aa66280
ML <emele18@gmail.com>
2016-11-15 21:15:35 -0500

    plotting is looking good!

--- lab5/plot.py
+++ lab5/plot.py
@@ -1,2 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
+f = open(sys.argv[1])
+time = []; values = []
+for line in f:
+    (timestamp, value) = line.strip().split()
+    time.append(timestamp)
+    values.append(value)
+
+time = map(float, time)
+time = [round(x - time[0],2) for x in time]
+values = map(int, values)
+
+plt.figure(1)
+plt.plot(time, values)
+plt.show()
