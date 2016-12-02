13d1c0ecb665ffda8b2bf04371deef3ba1d57b6f
eme-ele <emele18@gmail.com>
2016-12-02 18:55:01 -0500

    first hop is set

--- lab6/overlaybuild.c
+++ lab6/overlaybuild.c
@@ -1,2 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#define MAX_BUFF 2000
+
 int main(int argc, char *argv[]) {
+
+    // check that we have all needed params
+    if (argc < 6) {
+        printf("Run: %s [dst-IP] [dst-port] [routerk-IP, ...] [overlay-port] [build-port] [src-port]\n", argv[0]);
+        return -1;
+    }
+
+    int sd_router1, port_router1, n_bytes;
+    struct sockaddr_in addr_router1;
+    char buffer[MAX_BUFF + 1];
+    char message[MAX_BUFF + 1];
+    struct in_addr host_addr;
+    struct hostent *ipv4address;
+
+    inet_pton(AF_INET, argv[argc - 4], &host_addr);
+    printf("Router1 Hostname: %s\n", argv[argc - 4]);
+    ipv4address = gethostbyaddr(&host_addr, sizeof host_addr, AF_INET);
+    if (!ipv4address) {
+        perror("unknown address");
+        return -1;
+    }
+
+    socklen_t addrsize = sizeof(addr_router1);
+    printf("Router1 port: %s\n", argv[argc - 3]);
+    port_router1 = atoi(argv[argc - 3]);
+
+    // set router1 info
+    memset(&addr_router1, 0, sizeof(addr_router1));
+    addr_router1.sin_family = AF_INET;
+    bcopy(ipv4address->h_addr, &(addr_router1.sin_addr.s_addr), ipv4address->h_length);
+    addr_router1.sin_port = htons(port_router1);
+
+    if ((sd_router1 = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket error");
+        return -1;
+    }
+
+    message[0] = '\0';
+    snprintf(buffer, sizeof(buffer), "$%s$%s$", argv[1], argv[2]);
+    strcat(message, buffer);
+
+    int i;
+    for (i = 3; i < argc - 3; i++) {
+        snprintf(buffer, sizeof(buffer), "%s$", argv[i]);
+        strcat(message, buffer);
+    }
+
+    printf("%s\n", message);
+
+    n_bytes = sendto(sd_router1, message, strlen(message), 0, (struct sockaddr *) &addr_router1,
+                    sizeof(addr_router1));
+    printf("Sent: %d bytes\n", n_bytes);
+
+    n_bytes = recvfrom(sd_router1, buffer, sizeof(buffer), 0,
+                       (struct sockaddr *) &addr_router1, &addrsize);
+
+    if (n_bytes > 0) {
+        buffer[n_bytes] = '\0';
+        printf("%s\n", buffer);
+    }
+
 }
--- lab6/overlayrouter.c
+++ lab6/overlayrouter.c
@@ -5,6 +5,8 @@
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
 
 #define MAX_BUFF 2000
 
@@ -29,6 +31,7 @@ int main(int argc, char *argv[]) {
     char message[MAX_BUFF + 1];
 
     socklen_t addrsize = sizeof(addr_server);
+    port_server = atoi(argv[1]);
 
     // stuff to handle concurrency signals
     struct sigaction sigchld_action;
@@ -69,10 +72,76 @@ int main(int argc, char *argv[]) {
             k = fork();
 
             if (k == 0) {
-                buffer[n_bytes] = '\0';
-                // read data
-            }
+                char last_router[MAX_BUFF + 1];
+                char * ptr;
+                int beg_ip, count;
+                struct ifreq ifr;
+                char iface[] = "eth0";
+                char * localaddr;
 
+                ifr.ifr_addr.sa_family = AF_INET;
+                strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+                ioctl(sd_server, SIOCGIFADDR, &ifr);
+                localaddr = inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr);
+
+                printf("local address: %s\n", localaddr);
+
+                buffer[n_bytes] = '\0';
+                printf("%s\n", buffer);
+
+                buffer[n_bytes - 1] = '\0';
+                printf("%s\n", buffer);
+
+                ptr = strrchr(buffer, '$');
+                beg_ip = ptr - buffer + 1;
+                printf("beg_ip: %d\n", beg_ip);
+
+                memcpy(last_router, &buffer[beg_ip], strlen(buffer) - beg_ip);
+                last_router[strlen(buffer) - beg_ip] = '\0';
+                printf("last_router=%s\n", last_router);
+
+                if (strcmp(last_router, localaddr) == 0) {
+
+                    // first I need to return the ACK
+                    num_clients++;
+                    snprintf(message, sizeof(message), "%d", port_server + num_clients);
+                    sendto(sd_server, message, strlen(message), 0,
+                           (struct sockaddr *)&addr_server, sizeof(addr_server));
+
+                    printf("ACK: %s\n", message);
+
+                    // now I need to forward to the next in line
+                    memcpy(message, &buffer[0], strlen(buffer) - strlen(last_router));
+                    message[strlen(buffer) - strlen(last_router)] = '\0';
+                    printf("message: %s, length: %lu\n", message, strlen(message));
+
+                    // count separators to see if we are at the end of the chain
+                    int i;
+                    for(i = 0; i < strlen(message); i++) {
+                        if(message[i] == '$') {
+                            count++;
+                        }
+                    }
+
+                    printf("count of $: %d\n", count);
+                    if (count <= 3) {
+                        // i am the last router
+                    } else {
+                        memcpy(buffer, &message[0], strlen(message));
+                        buffer[strlen(message) - 1] = '\0';
+                        ptr = strrchr(buffer, '$');
+                        beg_ip = ptr - buffer + 1;
+                        printf("beg_ip: %d\n", beg_ip);
+
+                        memcpy(last_router, &buffer[beg_ip], strlen(buffer) - beg_ip);
+                        last_router[strlen(buffer) - beg_ip] = '\0';
+                        printf("next_router=%s\n", last_router);
+
+                        
+                    }
+
+                }
+            }
         }
 
     }
